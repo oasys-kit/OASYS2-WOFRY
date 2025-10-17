252451f26a703e494e93afb4eb5c403dddb06614
lrebuffi <lrebuffi@anl.gov>
2025-10-17 10:54:53 -0500

    Migration to Python 3.13 and PyQt6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS2-WOFRY'
-VERSION = '1.0.3'
+VERSION = '1.0.4'
 ISRELEASED = False
 
 DESCRIPTION = 'WOFRY (Wave Optics FRamework in pYthon)'
