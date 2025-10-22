a4275818d263dd763e8fe2227a4bae25f4ccd050
lrebuffi <lrebuffi@anl.gov>
2025-10-22 14:29:48 -0500

    Release of all the modification to Pypi

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS2-WOFRY'
-VERSION = '1.0.8'
+VERSION = '1.0.9'
 ISRELEASED = False
 
 DESCRIPTION = 'WOFRY (Wave Optics FRamework in pYthon)'
